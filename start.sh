140bb6f281cc57213cecaa870e4ad3c6184fea73
meoowe <96916064+meoowe@users.noreply.github.com>
2023-04-30 13:40:52 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "Hi! This script will install a wifi hotspot for you"
+if ! [ $(id -u) = 0 ]; then
+   echo "I am not root! Run me as root please :|"
+   exit 1
+fi
+echo "Starting your new hotspot!"
+systemctl start dnsmasq
+systemctl unmask hostapd
+systemctl enable hostapd
+systemctl start hostapd
+ip addr flush dev wlan0 2>&1 | show_debug
+ip addr add 192.168.150.1 dev wlan0
+# Enable routing
+sysctl net.ipv4.ip_forward=1 2
+# Enable NAT
+iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
